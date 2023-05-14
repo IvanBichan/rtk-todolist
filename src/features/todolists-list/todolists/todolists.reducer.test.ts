193699b1406c55b144862ae70d0ab74d81ed0a63
IvanBichan <ivanbichanfe@gmail.com>
2023-05-14 11:36:06 +0300

    refactor: change structure api for task and todolist

--- src/features/todolists-list/todolists/todolists.reducer.test.ts
+++ src/features/todolists-list/todolists/todolists.reducer.test.ts
@@ -5,7 +5,7 @@ import {
 } from 'features/todolists-list/todolists/todolists.reducer'
 import { v1 } from 'uuid'
 import { RequestStatusType } from 'app/app.reducer'
-import { TodolistType } from 'features/todolists-list/todolists.api'
+import { TodolistType } from 'features/todolists-list/todolists/todolists.api'
 
 let todolistId1: string
 let todolistId2: string
